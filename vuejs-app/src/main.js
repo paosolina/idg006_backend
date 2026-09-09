d918640209fab560fdfee01e8e4ca3f1f6beb9eb
Pao Solina <pao.solina088@gmail.com>
2026-09-09 14:19:01 +0700

    feat: implement user authentication flow with Pinia store, SweetAlert notifications, and Sign In/Sign Up pages

--- vuejs-app/src/main.js
+++ vuejs-app/src/main.js
@@ -1,17 +1,47 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "admin-lte/dist/js/adminlte.min.js";
 
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import "admin-lte/dist/js/adminlte.min.js"
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import App from "./App.vue";
+import router from "./router";
+import { useUserStore } from "@/stores/user";
+import { apiVerify } from "@/functions/api/auth";
 
+const app = createApp(App);
 
-const pinia = createPinia()
-const app = createApp(App)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+app.use(router);
+app.mount("#app");
 
-app.use(router)
-app.use(pinia)
+const userStore = useUserStore();
+router.beforeEach(async (to, from) => {
+    const { guarded } = to.meta;
+    if (guarded === undefined) {
+        // if the route is not guarded, we don't need to verify the token
+        return;
+    }
 
-app.mount('#app')
+    try {
+        const token = userStore.getSanctumToken();
+        const response = await apiVerify(token);
+        const { data } = response;
+        userStore.setState(data.user);
+    } catch (error) {
+        if (error.response && error.response.status === 401) {
+            userStore.reset();
+        }
+    }
+
+    if (guarded && !userStore.isAuthenticated) {
+        // if the route is guarded and the user is not authenticated, redirect to signin page
+        return { name: "auth.signin" };
+    }
+    if (!guarded && userStore.isAuthenticated) {
+        // if the route is not guarded and the user is authenticated, redirect to dashboard page
+        return { name: "dashboard" };
+    }
+});
--- vuejs-app/src/pages/Dashboard.vue
+++ vuejs-app/src/pages/Dashboard.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="alert alert-success" role="alert">
+        {{ userStore.name }}
+        <br />
+        {{ userStore.email }}
+        <br />
         You are logged in successfully. This is your dashboard.
         <RouterLink :to="{ name: 'auth.signout' }"
             ><i class="fas fa-sign-out-alt text-danger"></i
         ></RouterLink>
     </div>
 </template>
+<script setup>
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+</script>
