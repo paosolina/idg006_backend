2d15ba7e6c70e28cd7e2c5c5a83f28d3d43f4802
Pao Solina <pao.solina088@gmail.com>
2026-09-11 11:27:39 +0700

    feat: initialize vue-router configuration with authentication and dashboard routes

--- vuejs-app/src/router/index.js
+++ vuejs-app/src/router/index.js
@@ -3,27 +3,45 @@ import Signin from "@/auth/Signin.vue"
 import Dashboard from "@/pages/Dashboard.vue"
 import Signup from "@/auth/Signup.vue"
 import Signout from "@/auth/Signout.vue"
+import VerifyEmail from "@/auth/VerifyEmail.vue"
 
 const routes = [
     {
         path: "/",
         name: "auth.signin",
         component: Signin,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/dashboard",
         name: "dashboard",
         component: Dashboard,
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         path: "/signup",
         name: "auth.signup",
         component: Signup,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/signout",
         name: "auth.signout",
         component: Signout,
+    },
+    {
+        path: "/verify/email",
+        name: "auth.verify-email",
+        component: VerifyEmail,
+        meta: {
+            requiresAuth: false
+        }
     },
     {
         path: "/:pathMatch(.*)",
