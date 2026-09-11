0b701e19f45a6c8804b997048118fa8dc5293b8e
Pao Solina <pao.solina088@gmail.com>
2026-09-11 11:27:33 +0700

    feat: implement authentication routing and API service functions for user management

--- vuejs-app/src/functions/api/auth.js
+++ vuejs-app/src/functions/api/auth.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const APP_API_URL = import.meta.env.VITE_APP_API_URL;
+const APP_VERIFY_EMAIL_URL = import.meta.env.VITE_APP_VERIFY_EMAIL_URL;
 
 export async function apiSignUp(user) {
-    return await axios.post(APP_API_URL + "/signup", user);
+    return await axios.post(APP_API_URL + "/signup", {
+        ...user,
+        callback_url: APP_VERIFY_EMAIL_URL,
+    });
 }
 export async function apiSignIn(user) {
     return await axios.post(APP_API_URL + "/signin", user);
